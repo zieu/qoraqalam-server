fe7c52f7c20ba3ac3d0e00a6e1bff2d02e2353b6
Jamshidkhuja Burikhujaev <jamshidxojab@gmail.com>
2021-08-30 22:09:22 +0500

    added getUserByUsername method

--- src/User/User.ts
+++ src/User/User.ts
@@ -1,4 +1,3 @@
-import { Error } from "mongoose";
 import User from "./UserModel";
 
 export function UserModule() {
@@ -18,7 +17,23 @@ export function UserModule() {
     }
   };
 
+  const getUserByUsername = async (username: string) => {
+    try {
+      const user = await User.findOne({ username });
+      return {
+        success: true,
+        status: 200,
+      };
+    } catch (error) {
+      return {
+        success: false,
+        error: error.message,
+      };
+    }
+  };
+
   return {
     getUserById,
+    getUserByUsername,
   };
 }
