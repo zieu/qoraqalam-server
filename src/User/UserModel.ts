5420ef78468f43daa5b73af63119c3ac16768bf9
Zieu <zieu2000@gmail.com>
2021-08-30 21:07:37 +0500

    prettified

--- src/User/UserModel.ts
+++ src/User/UserModel.ts
@@ -2,61 +2,61 @@ import bcrypt from "bcryptjs";
 import { model, Schema, SchemaTypes } from "mongoose";
 
 interface IUser extends Document {
-    username: string;
-    email: string;
-    password: string;
-    avatar: string;
-    articles: string[];
-    starredArticles: string[];
-    createdAt: Date;
-    updatedAt: Date;
+  username: string;
+  email: string;
+  password: string;
+  avatar: string;
+  articles: string[];
+  starredArticles: string[];
+  createdAt: Date;
+  updatedAt: Date;
 }
 
 const UserSchema = new Schema<IUser>(
-    {
-        username: {
-            type: String,
-            required: true,
-            trim: true,
-            unique: true,
-        },
-        email: {
-            type: String,
-            required: true,
-            trim: true,
-            unique: true,
-        },
-        password: {
-            type: String,
-            required: true,
-        },
-        avatar: {
-            type: String,
-            default: undefined,
-        },
-        articles: [
-            {
-                type: SchemaTypes.ObjectId,
-                ref: "Article",
-            },
-        ],
-        starredArticles: [
-            {
-                type: SchemaTypes.ObjectId,
-                ref: "Article",
-            },
-        ],
+  {
+    username: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
     },
-    { timestamps: true },
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    avatar: {
+      type: String,
+      default: undefined,
+    },
+    articles: [
+      {
+        type: SchemaTypes.ObjectId,
+        ref: "Article",
+      },
+    ],
+    starredArticles: [
+      {
+        type: SchemaTypes.ObjectId,
+        ref: "Article",
+      },
+    ],
+  },
+  { timestamps: true },
 );
 
 UserSchema.pre("save", async function (next) {
-    if (this.isModified("password")) {
-        const hash = await bcrypt.hash(this.password, 12);
-        this.password = hash;
-        next();
-    }
+  if (this.isModified("password")) {
+    const hash = await bcrypt.hash(this.password, 12);
+    this.password = hash;
     next();
+  }
+  next();
 });
 
 export default model<IUser>("User", UserSchema);
