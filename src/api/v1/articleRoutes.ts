c69e40852cf5e96ce5491d066017c37f7b321133
Jamshidkhuja Burikhujaev <jamshidxojab@gmail.com>
2021-10-28 23:22:39 +0500

    added route to getting user articles

--- src/api/v1/articleRoutes.ts
+++ src/api/v1/articleRoutes.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
 
-import { createArticle, getArticleById } from "../../controllers/articleController";
+import { createArticle, getArticleById, getUserArticles } from "../../controllers/articleController";
 import { protect } from "../../controllers/authController";
 
 const router = Router();
 
 router.post("/", protect, createArticle);
 router.get("/:id", protect, getArticleById);
+router.get("/user/articles", getUserArticles);
 
 export default router;
